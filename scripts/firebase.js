3d49380a525ed5db8e4bb1450c931b843f8d24b4
mgijima <mgi0x1d@gmail.com>
2026-01-18 17:34:04 +0200

    update phone field

--- scripts/firebase.js
+++ scripts/firebase.js
@@ -2,10 +2,10 @@ const firebaseConfig = {
   apiKey: "AIzaSyC_QPmomjx6YmP55SiorvkODVHf7-z8mTA",
   authDomain: "digilayn-core-app.firebaseapp.com",
   projectId: "digilayn-core-app",
-  storageBucket: "digilayn-core-app.appspot.com",
+  storageBucket: "digilayn-core-app.firebasestorage.app",
   messagingSenderId: "362012916883",
-  appId: "1:362012916883:web:577fa096e6701656912166",
-  measurementId: "G-5JZ99E9GFB"
+  appId: "1:362012916883:web:3a3d64ee448c536e912166",
+  measurementId: "G-13CS1P7XJ2"
 };
 
 // Initialize Firebase
